42ace20fed50d5f8f186ac70d1ba48497900c9df
Phil Brockwell <nineteen40eight@gmail.com>
2015-02-04 17:32:06 +0000

    started van tests

--- lib/van.rb
+++ lib/van.rb
@@ -1,2 +1,10 @@
+require_relative 'bike_container'
+
 class Van
+
+  include BikeContainer
+
+  def get_broken_bikes(station)
+    #need to create bikeContainer method to select broken bikes
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,12 +3,24 @@
 # It can also be used to take fixed bikes back to the station(s).
 # The van, all stations and the garage have fixed capacity, so they cannot take more bikes that they can hold.
 
+
+
 require 'van'
 
 describe Van do
 
-  it 'should do something' do
+  it 'should take all broken bikes from a station' do
+    van = Van.new
+    station = double :station, dock: nil, available_bikes: nil
+    bike = double :bike, broken: false
+    broken_bike = double :bike, broken: true
 
+    # expect(station).to receive(:dock)
+
+    2.times {station.dock(bike)}
+    2.times {station.dock(broken_bike)}
+    van.get_broken_bikes(station)
+    expect(van.bikes).to be_eq [broken_bike, broken_bike]
   end
   
 end
