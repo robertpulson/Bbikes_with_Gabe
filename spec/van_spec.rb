e843f2d7d5b92bd07cacb1eb9a487670cc31d0bf
gabe <GabeMaker@users.noreply.github.com>
2015-02-04 15:50:23 +0000

    add blank van spec and file

---
+++ spec/van_spec.rb
@@ -0,0 +1,14 @@
+# A van is used to move broken bikes from the stations to the garage.
+
+# It can also be used to take fixed bikes back to the station(s).
+# The van, all stations and the garage have fixed capacity, so they cannot take more bikes that they can hold.
+
+require 'van'
+
+describe Van do
+
+  it 'should do something' do
+
+  end
+  
+end
